653b4d771e8445764293a6cfd64366aeb41d9bd2
Markov Alexey <markovalex95@gmail.com>
2017-02-09 00:28:21 +0300

    Добавьте файлы проекта.

---
+++ GasStantion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GasStantion.Startup))]
+namespace GasStantion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
