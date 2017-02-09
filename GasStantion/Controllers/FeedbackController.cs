c170f58fdd7558cc1f53d657ab3d88b2b816b818
Markov Alexey <markovalex95@gmail.com>
2017-02-10 00:25:17 +0300

    Добавил страницу отзывов и оставление нвого отзыва

---
+++ GasStantion/Controllers/FeedbackController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GasStantion.EntityFramework;
+using GasStantion.Models;
+
+namespace GasStantion.Controllers
+{
+    //Просмотр отзывыво
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult Index()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var model = context.UserFeedbacks.OrderByDescending(x => x.Id).ToList();
+                return View(model);
+            }
+        }
+
+        //Создание нового отзывы
+        [HttpGet]
+        public ActionResult New()
+        {
+            return View(new UserFeedback());
+        }
+
+        public ActionResult New(UserFeedback model)
+        {
+            if(!ModelState.IsValid)
+                return View(model);
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.UserFeedbacks.Add(model);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
