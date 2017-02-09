1b834d61e12e03bc88c33cedfb2d3cfc57516608
Markov Alexey <markovalex95@gmail.com>
2017-02-10 02:00:12 +0300

    Добавил админку

---
+++ GasStantion/Areas/Admin/Util/PathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace GasStantion.Areas.Admin.ViewModels
+{
+    public static class PathHelper
+    {
+        public static string PathKey => "__path";
+
+        public static void AddPath(this ViewDataDictionary<dynamic> viewData, string url, string title)
+        {
+            if(viewData==null)
+                return;
+
+            if (viewData[PathKey] == null)
+                viewData[PathKey] = new List<PathPart>();
+
+            (viewData[PathKey] as List<PathPart>).Add(new PathPart(url, title));
+        }
+    }
+}
---
+++ GasStantion/Areas/Admin/Util/PathPart.cs
@@ -0,0 +1,20 @@
+﻿namespace GasStantion.Areas.Admin.ViewModels
+{
+    /// <summary>
+    /// Элемент "пути" в админке, чтобы делать удобную навигацию
+    /// </summary>
+    public class PathPart
+    {
+        public string Url { get; set; }
+        public string Text { get; set; }
+
+        public PathPart() { }
+
+        public PathPart(string url, string text)
+        {
+            Url = url;
+            Text = text;
+        }
+
+    }
+}
