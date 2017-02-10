d430bfdd08849a6d0209b1b4e1b98664a70b91e1
Markov Alexey <markovalex95@gmail.com>
2017-02-10 03:11:12 +0300

    Сделал в админке топливо

--- GasStantion/Areas/Admin/Controllers/AdminFuelController.cs
+++ GasStantion/Areas/Admin/Controllers/AdminFuelController.cs
@@ -11,92 +11,93 @@ using GasStantion.Models;
 
 namespace GasStantion.Areas.Admin.Controllers
 {
-    public class AdminNewsController : Controller
+    public class AdminFuelController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Admin/News
+        // GET: Admin/AdminFuel
         public ActionResult Index()
         {
-            return View(db.News.ToList());
+            return View(db.Fuels.ToList());
         }
 
-        // GET: Admin/News/Create
+
+        // GET: Admin/AdminFuel/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/News/Create
+        // POST: Admin/AdminFuel/Create
         // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
         // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Title,Text,PreviewImageUrl")] News news)
+        public ActionResult Create([Bind(Include = "Id,FuelName,Price,FuelDescription")] Fuel fuel)
         {
             if (ModelState.IsValid)
             {
-                db.News.Add(news);
+                db.Fuels.Add(fuel);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(news);
+            return View(fuel);
         }
 
-        // GET: Admin/News/Edit/5
+        // GET: Admin/AdminFuel/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            News news = db.News.Find(id);
-            if (news == null)
+            Fuel fuel = db.Fuels.Find(id);
+            if (fuel == null)
             {
                 return HttpNotFound();
             }
-            return View(news);
+            return View(fuel);
         }
 
-        // POST: Admin/News/Edit/5
+        // POST: Admin/AdminFuel/Edit/5
         // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
         // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,Text,PreviewImageUrl")] News news)
+        public ActionResult Edit([Bind(Include = "Id,FuelName,Price,FuelDescription")] Fuel fuel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(news).State = EntityState.Modified;
+                db.Entry(fuel).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(news);
+            return View(fuel);
         }
 
-        // GET: Admin/News/Delete/5
+        // GET: Admin/AdminFuel/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            News news = db.News.Find(id);
-            if (news == null)
+            Fuel fuel = db.Fuels.Find(id);
+            if (fuel == null)
             {
                 return HttpNotFound();
             }
-            return View(news);
+            return View(fuel);
         }
 
-        // POST: Admin/News/Delete/5
+        // POST: Admin/AdminFuel/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            News news = db.News.Find(id);
-            db.News.Remove(news);
+            Fuel fuel = db.Fuels.Find(id);
+            db.Fuels.Remove(fuel);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
